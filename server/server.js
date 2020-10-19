1017f2306c5e6b68ba778c3d2a14327bec8a94df
Jon <jrohrbac@net.ist.ucf.edu>
2020-10-19 16:36:31 -0400

    Pre-deployment MERN tutorial build

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser');
 
 const app = express();
 const port = process.env.PORT || 5000;
+const mongoose = require('mongoose');
 
 // use body parser to get data from POST requests
 app.use(bodyParser.json());
@@ -12,5 +13,11 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // use API routes from the api folder
 const apis = require("./api");
 app.use("/api", apis);
+
+// Connect to mongo
+mongoose.connect(process.env.DB, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}).then(() => console.log('MongoDB Connected...')).catch(err => console.log(err));
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
